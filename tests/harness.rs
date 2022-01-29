3e2e8b05c01dd707d533e81db97463be5d833d5b
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2022-01-28 22:05:15 -0700

    Update harness.rs

--- tests/harness.rs
+++ tests/harness.rs
@@ -4,11 +4,16 @@ use fuels_rs::contract::Contract;
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
 
-// Generate Rust bindings from our contract JSON ABI
 abigen!(MyContract, "./my-contract-abi.json");
 
-#[tokio::test]
-async fn harness() {
+pub struct DepositInput {
+    input_token:[u8; 32],
+    from:[u8; 32],
+    to:[u8; 32],
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn mapping_test() {
     let rng = &mut StdRng::seed_from_u64(2322u64);
 
     // Build the contract
@@ -17,26 +22,82 @@ async fn harness() {
     let compiled = Contract::compile_sway_contract("./", salt).unwrap();
 
     // Launch a local network and deploy the contract
-    let (client, contract_id) = Contract::launch_and_deploy(&compiled).await.unwrap();
+    let (client, _contract_id) = Contract::launch_and_deploy(&compiled).await.unwrap();
 
     let contract_instance = MyContract::new(compiled, client);
 
+    let inputToken_input:[u8; 32] = [0; 32];
+    
+    let from_input:[u8; 32] = [5; 32];
+
+    let to_input:[u8; 32] = [7; 32];
+
     // Call `initialize_counter()` method in our deployed contract.
     // Note that, here, you get type-safety for free!
-    let result = contract_instance
-        .sqrtu32(100)
+    let _result = contract_instance
+        .test_store(100, 100, [0; 32], 100)
         .call()
         .await
         .unwrap();
 
-    assert_eq!(0, result);
-
-    // Call `increment_counter()` method in our deployed contract.
     let result = contract_instance
-        .sqrtu64(15)
+        .test_retrieve(100, 100, [0; 32], 100)
         .call()
         .await
         .unwrap();
 
-    assert_eq!(5, result);
-}
+    assert_eq!(100, result);
+    
+    let _result = contract_instance
+        .test_rebase_map_store(100, 100, [0; 32], 100)
+        .call()
+        .await
+        .unwrap();
+
+    let result = contract_instance
+        .test_rebase_map_retrieve(100, 100, [0; 32], 100)
+        .call()
+        .await
+        .unwrap();
+
+    assert_eq!(110, result);
+} 
+
+/*
+#[tokio::test(flavor = "multi_thread")]
+async fn rebase_test() {
+    let rng = &mut StdRng::seed_from_u64(2322u64);
+
+    // Build the contract
+    let salt: [u8; 32] = rng.gen();
+    let salt = Salt::from(salt);
+    let compiled = Contract::compile_sway_contract("./", salt).unwrap();
+
+    // Launch a local network and deploy the contract
+    let (client, _contract_id) = Contract::launch_and_deploy(&compiled).await.unwrap();
+
+    let contract_instance = MyContract::new(compiled, client);
+
+    let inputToken_input:[u8; 32] = [0; 32];
+    
+    let from_input:[u8; 32] = [5; 32];
+
+    let to_input:[u8; 32] = [7; 32];
+
+    // Call `initialize_counter()` method in our deployed contract.
+    // Note that, here, you get type-safety for free!
+
+    let _result = contract_instance
+        .test_rebase_map_store(100, 100, [0; 32], 100)
+        .call()
+        .await
+        .unwrap();
+
+    let result = contract_instance
+        .test_rebase_map_retrieve(100, 100, [0; 32], 100)
+        .call()
+        .await
+        .unwrap();
+
+    assert_eq!(110, result);
+} */
